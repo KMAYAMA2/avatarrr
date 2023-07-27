fb97f46b61cb61327d26946f0e9c55f5c2554196
Kento Mayama <c191246x@gmail.com>
2023-07-28 07:35:42 +0900

    to be deployed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,120 @@
+// Add useState import to top of file
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
+
+
 const Home = () => {
+  // Create state property
+  const [input, setInput] = useState('');
+  // Create new state property
+  const [img, setImg] = useState(''); 
+  // Don't retry more than 20 times
+  const maxRetries = 20;
+  // Numbers of retries 
+  const [retry, setRetry] = useState(0);
+  // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  // Add isGenerating state
+  const [isGenerating, setIsGenerating] = useState(false);
+  // Add new state here
+  const [finalPrompt, setFinalPrompt] = useState('');
+
+  // Add this function
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  // Add generateAction
+  const generateAction = async () => {
+    console.log('Generating...');	
+
+    // Add this check to make sure there is no double click
+    if (isGenerating && retry === 0) return;
+      
+    // Set loading has started
+    setIsGenerating(true);
+
+    // If this is a retry request, take away retryCount
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+
+    // Add the fetch request
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    // If model still loading, drop that retry time
+    if (response.status === 503) {
+      console.log('Model is loading still :(.')
+      return;
+    }
+
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      // Stop loading
+      setIsGenerating(false);
+      return;
+    }
+
+    // Set final prompt here
+    setFinalPrompt(input);
+    // Remove content from input box
+    setInput('');
+    // Set image data into state property
+    setImg(data.image);
+    // Everything is all done -- stop loading!
+    setIsGenerating(false);
+  }
+
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  // Add useEffect here
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+        }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +123,45 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Silly picture generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "chanakakento man" in the prompt</h2>
           </div>
+          {/* Add prompt container here */}
+          <div className="prompt-container">
+            {/* Add value property */}
+            <input className="prompt-box" value={input} onChange={onChange} />
+            
+            {/* Add your prompt button in the prompt container */}
+            <div className="prompt-buttons">
+              {/* Add onClick property here */}
+              <a 
+                className={
+                  isGenerating ? 'generate-button loading' : "generate-button"
+                 }
+                onClick={generateAction}
+              >
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
+          </div>        
         </div>
+
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={finalPrompt} />
+            {/* Add prompt here */}
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
